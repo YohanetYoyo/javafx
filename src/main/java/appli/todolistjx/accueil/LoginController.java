9f3a67eff61a678a5e9b67e003f728e1747fd008
Yohan VIGNEZ <VIGNEZ_Yo@lprs.local>
2024-09-17 15:23:09 +0200

    Création méthodes email et mdp pour connexion

--- src/main/java/appli/todolistjx/accueil/LoginController.java
+++ src/main/java/appli/todolistjx/accueil/LoginController.java
@@ -8,12 +8,23 @@ import javafx.scene.control.TextField;
 public class LoginController {
     @FXML
     private Label erreur;
+    @FXML
     private TextField emailField;
+    @FXML
     private PasswordField passwordField;
+
+    @FXML
+    protected void setEmailField(TextField textField){
+    }
+    @FXML
+    protected void setPasswordField(PasswordField passwordField){
+    }
 
     @FXML
     protected void connexion() {
         System.out.println("Connexion");
+        System.out.println(this.emailField.toString());
+        System.out.println(this.passwordField.toString());
     }
     @FXML
     protected void motDePasseOublie() {
