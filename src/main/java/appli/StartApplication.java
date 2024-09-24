b55d95299e87d054241bd1380a1f72d20dae2731
Yohan VIGNEZ <VIGNEZ_Yo@lprs.local>
2024-09-24 15:01:39 +0200

    Objectif 8 : Créer une méthode gloable permettant de changer d’interface

--- src/main/java/appli/StartApplication.java
+++ src/main/java/appli/StartApplication.java
@@ -8,13 +8,22 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class StartApplication extends Application {
+    private static Stage mainStage;
+
+    private static void changeScene(String nomDuFichierFxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource(nomDuFichierFxml));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        mainStage.setScene(scene);
+    }
+    
     @Override
     public void start(Stage stage) throws IOException {
+        mainStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("accueil/loginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        mainStage.setTitle("Hello!");
+        mainStage.setScene(scene);
+        mainStage.show();
     }
 
     public static void main(String[] args) {
