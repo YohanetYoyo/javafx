521c134b59ba8d4f2265a335147fcaa0976373c3
Yohan VIGNEZ <VIGNEZ_Yo@lprs.local>
2024-10-08 13:59:04 +0200

    3 - Modification de la page accueilView.fxml, actualisation de AccueilControleur et implémentation de l'interface dans AccueilControleur

--- src/main/java/appli/accueil/AccueilController.java
+++ src/main/java/appli/accueil/AccueilController.java
@@ -1,10 +1,29 @@
 package appli.accueil;
 
 import appli.StartApplication;
-import java.io.IOException;
+import javafx.fxml.Initializable;
 
-public class AccueilController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AccueilController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+
+    public void ajouterListe() {
+
+    }
+
+    public void lesTypes(){
+
+    }
+
     public void deconnexion() throws IOException {
         StartApplication.changeScene("accueil/loginView.fxml");
     }
+
 }
