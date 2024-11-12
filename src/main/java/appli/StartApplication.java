ccd1b5441c539d55cc27cd9f533ba9d693d728c4
Yohan VIGNEZ <yohanvignez@gmail.com>
2024-11-12 21:12:01 +0100

    Résolution fixe désormais plus grande pour chaque scène

--- src/main/java/appli/StartApplication.java
+++ src/main/java/appli/StartApplication.java
@@ -15,15 +15,15 @@ public class StartApplication extends Application {
     public void start(Stage stage) throws IOException {
         mainStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("accueil/loginView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        mainStage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 550, 600);
+        mainStage.setTitle("Projet JavaFX");
         mainStage.setScene(scene);
         mainStage.show();
     }
 
     public static void changeScene(String nomDuFichierFxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource(nomDuFichierFxml));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 550, 600);
         mainStage.setScene(scene);
     }
 
